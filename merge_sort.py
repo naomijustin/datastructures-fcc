e15aed1f7433a89b87ce14ef3b2b16829ebd10b7
naomijustin <naomi.z.justin@gmail.com>
2025-03-06 15:28:09 +1100

    Add doc string explaining steps of merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,1 +1,9 @@
-def merge_sort(list): """ starting function definition """
+def merge_sort(list): 
+    """ 
+    Sorts a list in ascending order 
+    Returns a new sorted list 
+
+    Divide: Find the midpoint of the list and divide into sublists
+    Conquer: Recursively sorts the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+    """
