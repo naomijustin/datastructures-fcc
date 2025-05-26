5e1287b7ed866a199bc532ca8b47d09882e2c803
naomijustin <naomi.z.justin@gmail.com>
2025-05-27 07:50:15 +1000

    document new merge_sort function with steps

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -1,6 +1,10 @@
 from linked_list import LinkedList
 
-l = LinkedList()
-l.add(1)
+def merge_sort(linked_list):
+    """
+    Sorts a linked list in ascending order 
+    - Recursively divide the linked list into sublists containing a single node
+    - Repeatedly merge the sublists to produce sorted sublists until one remains
 
-print(l)
+    Returns a sorted linked list
+    """
